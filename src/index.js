7e6d3c3a147d18f4ccad31f4bc3b2e0da184da7f
Nicolas Nuccelli <nuccelli@hotmail.com>
2022-02-12 01:13:58 -0300

    Trabajando en props y Spring Operator

--- src/index.js
+++ src/index.js
@@ -2,17 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/817-Vrzp+tL._AC_UL604_SR604,400_.jpg",
-  title: "Little Blue Truck's Valentine",
-  autor: "Alice Sohertle",
-};
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL604_SR604,400_.jpg",
-  title: "The 40 Laws of Power",
-  autor: "Peter Greene",
-};
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/817-Vrzp+tL._AC_UL604_SR604,400_.jpg",
+    title: "Little Blue Truck's Valentine",
+    autor: "Alice Sohertle",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL604_SR604,400_.jpg",
+    title: "The 40 Laws of Power",
+    autor: "Peter Greene",
+  },
+];
+function Booklist() {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
+}
+//Spring operator ... llama a las popiedades por separado de un objeto
 
+const Book = ({ img, title, autor }) => {
+  //  const { img, title, autor } = props.book;
+
+  return (
+    <article className="book">
+      <img id="image" src={img} alt="" />
+      <h4>{title}</h4>
+      <h5>{autor}</h5>
+    </article>
+  );
+};
+// Siempre que haya una lista hay que incluir el key prop
+/*
 function Booklist() {
   return (
     <div className="booklist">
@@ -43,5 +70,5 @@ let Book = ({ img, title, autor, children }) => {
     </article>
   );
 };
-
+*/
 ReactDOM.render(<Booklist />, document.getElementById("root"));
